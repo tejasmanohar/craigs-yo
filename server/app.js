130db790a2263bd580fbd80ff9a932a08674f1c0
Tejas Manohar <me@tejasmanohar.com>
2014-08-07 11:13:17 -0500

    irc help

--- server/app.js
+++ server/app.js
@@ -53,7 +53,8 @@ handleSubscriber();
 function handleSubscriber() {
   request('http://www.google.com', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body)
+      console.log(body);
+      console.log(arguments);
     }
   });
 };
