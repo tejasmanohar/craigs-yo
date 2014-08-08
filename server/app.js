2d3f37ab3a08595f03d04c27f0bc35f07e9a7911
Tejas Manohar <me@tejasmanohar.com>
2014-08-08 15:20:39 -0500

    v1 release time

--- server/app.js
+++ server/app.js
@@ -3,10 +3,8 @@
  */
 
 'use strict';
-
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 var _ = require('lodash');
 var async = require('async');
 var express = require('express');
@@ -14,59 +12,90 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
 var request = require('request');
-
 process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err);
 });
 
-// Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
-
 setTimeout(function () {
   console.log('This will still run.');
 }, 500);
 
-var Subscriber = mongoose.model('Subscriber', new mongoose.Schema({
-  yo: {
-    type: String,
-    unique: true
-  },
-  url: String,
-  content: String
-}));
+var s = {
+  Subscriber : mongoose.model('Subscriber', new mongoose.Schema({
+    yo: {
+      type: String,
+      unique: true
+    },
+    url: String,
+    body: String
+  })),
+  getPage : function(url, cb) {
+    request(url, cb);
+  }
+};
 
-// Setup server
+
 var app = express();
 var server = require('http').createServer(app);
-
 require('./config/express')(app);
-require('./routes')(app);
+require('./routes')(app, s);
 
-// Yo stuff
 var yo = (function() {
   var Yo = require('yo-api');
   return new Yo(process.env.YO_API_TOKEN);
 })();
 
-// function updateSubscribers() {
-//   Subscriber.find().exec(function(err, docs) {
-//     async.each(docs, function(doc, next) {
-//       handleSubscriber(doc, next);
-//     }, function(err) {
-//       if (err) {
-//         console.log(err);
-//       }
-//       console.log('Updated ' + docs.length + ' subscribers.');
-//     });
-//   });
-// }
-// setInterval(updateSubscribers, 60000);
-// updateSubscribers();
 
-// Start server
+var updateSubscription = function(){
+  var i, entry;
+  s.Subscriber
+    .find(function (err, doc) {
+      if(doc)
+      {
+        for(i = 0; i< doc.length; i++){
+          entry = doc[i];
+          s.getPage(entry.url,
+            function (entry,error, response, body) {
+              if (!error && response.statusCode == 200)
+              {
+                if(body != entry.body)
+                {
+                  entry.body = body;
+                  entry.save(function(err) {
+                    if(!err)
+                    {
+                      console.log('OK');
+                    }
+                    else
+                    {
+                      console.log(err);
+                    }
+                  });
+                  console.log('Found new listings for user ' + entry.yo + ' !');
+                  yo.yo(entry.yo, function() {
+                    console.log("Yo'ed user" + entry.yo + ' !');
+                  });
+                }
+                else
+                {
+                  console.log('You are now subscribed!');
+                }
+              }
+              else
+              {
+                console.log(error);
+              }
+            }.bind(this, entry)
+          );
+        }
+      }
+    });
+};
+
+var interval = setInterval(updateSubscription, 61000);
+
+
 server.listen(config.port, config.ip, function() {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
-
-// Expose model
-exports.Subscriber = Subscriber;
--- server/routes.js
+++ server/routes.js
@@ -4,71 +4,100 @@
 
 'use strict';
 
-var errors = require('./components/errors');
-var mongoose = require('mongoose');
-var request = require('request');
-var Subscriber = require('./app').Subscriber;
+module.exports = function(app,s){
+  var errors = require('./components/errors');
 
-function handleNewSubscriber(url, yoName, cb) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var doc = new Subscriber({
-            yo: yoName.toLowerCase(),
-            url: url,
-            body: body
+  app.post('/register', function (req, res) {
+    var yoName = req.body.yoName.toLowerCase(), link = req.body.link;
+
+    s.Subscriber
+      .findOne({"yo" : yoName}, function (err, doc) {
+        console.log(err, doc);
+        if(doc)
+        {
+          s.getPage(link,
+            function (error, response, body) {
+              if (!error && response.statusCode == 200)
+              {
+                if(doc.url !== link)
+                {
+                  doc.body = body;
+                  doc.save(function(err) {
+                    if(!err)
+                    {
+                      res.send('OK');
+                    }
+                    else
+                    {
+                      res.send(err);
+                    }
+                  });
+                }
+                else
+                {
+                  res.send('You are already subscribed for this address');
+                }
+              }
+              else
+              {
+                res.send(error);
+              }
+            }
+          );
+        }
+        else
+        {
+          s.getPage(link,
+            function (error, response, body) {
+              if (!error && response.statusCode == 200)
+              {
+                var doc = new s.Subscriber({
+                  yo: yoName,
+                  url: link,
+                  body: body
+                });
+
+                doc.save(function(err) {
+                  if(!err)
+                  {
+                    res.send('OK');
+                  }
+                  else
+                  {
+                    res.send(err);
+                  }
+                });
+              }
+              else
+              {
+                res.send(error);
+              }
+            }
+          );
+        }
       });
-      cb(null, doc);
-    }
-  });
-};
-
-module.exports = function(app) {
-  app.route('/register')
-    .post(function(req, res) {
-      console.log('/register');
-      var yoName = req.body.yoName;
-      var link = req.body.link;
-      Subscriber.findOne({
-        yo: yoName
-      }).exec(function(err, doc) {
-        console.log(err);
-        if (!doc) {
-          handleNewSubscriber(link, yoName.toLowerCase(),
-            function (err, newDoc) {
-              newDoc.save(function(err) {
-                res.send('OK');
-              });
-          });
-        } else {
-          doc.following.push(link);
-          doc.save(function(err) {
-            res.send('OK');
-          });
-        }
-    });
   });
 
-  app.route('/unsubscribe')
-    .get(function(req, res) {
-      var username = req.query.username.toLowerCase();
-      Subscriber.remove({
-        yo: username
-      }).exec(function(err) {
-        if (err) {
-          res.status(404).send('Username does not exist');
-          return;
-        }
+  app.get('/unsubscribe', function (req, res) {
+    var yoName = req.query.username.toLowerCase();
+    s.Subscriber.remove({"yo" : yoName}, function (err, result) {
+      if(result)
+      {
         res.send('OK');
-      });
+      }
+      else
+      {
+        res.status(404).send('Username does not exist');
+      }
     });
+  });
 
-  // All undefined asset or api routes should return a 404
+
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
     .get(errors[404]);
 
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
-    });
+
+  app.route('/*').get(function(req, res) {
+    res.sendfile(app.get('appPath') + '/index.html');
+  });
 };
