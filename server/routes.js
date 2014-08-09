0a60725a9bbbf0aebb1c19de703bd1fd1682210a
Ian Macalinao <ian@giza.us>
2014-08-08 23:02:05 -0500

    Fix bugs
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- server/routes.js
+++ server/routes.js
@@ -4,81 +4,66 @@
 
 'use strict';
 
-module.exports = function(app,s){
+module.exports = function(app, s) {
   var errors = require('./components/errors');
 
-  app.post('/register', function (req, res) {
-    var yoName = req.body.yoName.toLowerCase(), link = req.body.link;
+  app.post('/register', function(req, res) {
+    var yoName = req.body.yoName.toLowerCase(),
+      link = req.body.link;
 
     s.Subscriber
-      .findOne({"yo" : yoName}, function (err, doc) {
+      .findOne({
+        "yo": yoName
+      }, function(err, doc) {
         console.log(err, doc);
-        if(doc)
-        {
+        if (doc) {
           s.getPage(link,
-            function (error, response, body) {
-              if (!error && response.statusCode == 200)
-              {
-                if(doc.url !== link)
-                {
+            function(error, response, body) {
+              if (!error && response.statusCode == 200) {
+                if (doc.url !== link) {
                   doc.body = body;
                   doc.save(function(err) {
-                    if(!err)
-                    {
+                    if (!err) {
                       console.log("heloo");
                       res.send('OK');
-                    }
-                    else
-                    {
+                    } else {
                       console.log("qwerty");
                       res.send(err);
                     }
                   });
-                }
-                else
-                {
+                } else {
                   console.log("jklm");
                   res.send('You are already subscribed for this address');
                 }
-              }
-              else
-              {
+              } else {
                 console.log("zxc");
                 res.send(error);
               }
             }
           );
-        }
-        else
-        {
+        } else {
           s.getPage(link,
-            function (error, response, body) {
-              if (!error && response.statusCode == 200)
-              {
+            function(error, response) {
+              if (!error && response.statusCode == 200) {
                 console.log("testtestestest");
                 var doc = new s.Subscriber({
                   yo: yoName,
                   url: link,
-                  body: body
+                  body: response.body
                 });
 
                 doc.save(function(err) {
-                  if(!err)
-                  {
+                  if (!err) {
                     console.log("hereeee");
                     res.send('OK');
-                  }
-                  else
-                  {
+                  } else {
                     console.log("asdf");
                     res.send(err);
                   }
                 });
-              }
-              else
-              {
+              } else {
+                console.log(error);
                 console.log(response);
-                console.log(body);
                 res.send(error);
               }
             }
@@ -87,15 +72,14 @@ module.exports = function(app,s){
       });
   });
 
-  app.get('/unsubscribe', function (req, res) {
+  app.get('/unsubscribe', function(req, res) {
     var yoName = req.query.username.toLowerCase();
-    s.Subscriber.remove({"yo" : yoName}, function (err, result) {
-      if(result)
-      {
+    s.Subscriber.remove({
+      "yo": yoName
+    }, function(err, result) {
+      if (result) {
         res.send('OK');
-      }
-      else
-      {
+      } else {
         res.status(404).send('Username does not exist');
       }
     });
